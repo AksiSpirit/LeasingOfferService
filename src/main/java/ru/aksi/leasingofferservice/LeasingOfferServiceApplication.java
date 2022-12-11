3cdc5108c8a593fc402dcef55cbe86b480a31c71
AksiSpirit <mewsim1000@mail.ru>
2022-12-11 18:37:22 +0300

    Initial commit

---
+++ src/main/java/ru/aksi/leasingofferservice/LeasingOfferServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.aksi.leasingofferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasingOfferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasingOfferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aksi/leasingofferservice/LeasingOfferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aksi.leasingofferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingOfferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
