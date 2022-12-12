3befba4df6be3265446a4bc13bd357ebb3f2560f
AksiSpirit <mewsim1000@mail.ru>
2022-12-12 23:23:42 +0300

    idk

--- src/main/java/ru/aksi/leasingofferservice/service/StatisticsService.java
+++ src/main/java/ru/aksi/leasingofferservice/service/StatisticsService.java
@@ -1,6 +1,7 @@
 package ru.aksi.leasingofferservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.aksi.leasingofferservice.dto.OfferStatistics;
 import ru.aksi.leasingofferservice.model.Offer;
 import ru.aksi.leasingofferservice.repository.ClientRepository;
@@ -9,6 +10,7 @@ import ru.aksi.leasingofferservice.repository.OfferRepository;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Service
 public class StatisticsService {
     private OfferRepository offerRepository;
     private ClientRepository clientRepository;
